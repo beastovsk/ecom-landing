8267f884d8060c577c5a7a43d987fe307322c0ef
Artyom Narshinov <narshinovartyom@mail.ru>
2025-03-29 01:50:26 +0400

    upd

--- app/layout.tsx
+++ app/layout.tsx
@@ -145,8 +145,8 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           }
         })}
       </Script>
-      <Script>
-       {`(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h;var p = d.location.protocol == "https:" ? "https://" : "http://";var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '466b219bcd9a5880c2c88fa4c292c3d9')`}
+      <Script type='text/javascript'>
+        {`(function(w, d, s, h, id) {w.roistatProjectId = id; w.roistatHost = h;var p = d.location.protocol == "https:" ? "https://" : "http://";var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '466b219bcd9a5880c2c88fa4c292c3d9')`}
       </Script>
     </html>
   );
